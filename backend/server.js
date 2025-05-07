657d7beed9b63f11450e781aae490b682accd3c5
subham-22 <balsubham59@gmail.com>
2025-05-07 22:30:01 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ connectCloudinary();
 app.use(express.json())
 const allowedOrigins = [
     "https://quiet-hummingbird-0c190c.netlify.app", // admin panel
-    "https://app.netlify.com" // frontend site
+    "hhttps://foreverjwell.netlify.app/" // frontend site
   ];
   
   
